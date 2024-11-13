7912c054e68aa14a7b507bf7ef76d4960c1ad307
adhimrahman <adhimrahaman@gmail.com>
2024-11-13 23:27:25 +0800

    add: signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,12 +1,45 @@
+import { useState } from 'react';
+import { auth } from '../../firebase/';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import SignupIndividu from '../components/signupIndividu';
 
-function Signup() {
-    console.log("Signup loaded");
+export default function Signup() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [fullName, setFullName] = useState('');
+	const [errorMessage, setErrorMessage] = useState(null);
 
-	return(
-		<>
-			<h1 className="text-black">LingkunganEvent</h1>
-		</>
-	)
+	const navigate = useNavigate();
+
+	const handleSignup = async (event) => {
+		event.preventDefault();
+		if (password !== confirmPassword) {
+			return setErrorMessage("Passwords do not match");
+		}
+		try {
+			await createUserWithEmailAndPassword(auth, email, password);
+			navigate('/individu'); // Or redirect to another page
+		} catch (error) {
+			setErrorMessage(error.message);
+		}
+	};
+
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bege.png')] bg-center bg-cover">
+			<SignupIndividu 
+				handleSignup={handleSignup} 
+				setEmail={setEmail} 
+				setPassword={setPassword} 
+				setConfirmPassword={setConfirmPassword}
+				setFullName={setFullName} 
+				email={email} 
+				password={password} 
+				confirmPassword={confirmPassword} 
+				fullName={fullName} 
+			/>
+			{errorMessage && <p className="text-red-500 mt-4">{errorMessage}</p>}
+		</div>
+	);
 }
-
-export default Signup
