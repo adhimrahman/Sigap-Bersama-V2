464bd7174510f665e3d28f80bf9bcf6f0716f539
adhimrahman <adhimrahaman@gmail.com>
2024-11-13 18:15:46 +0800

    add: setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import IndividuPage from "./pages/IndividuPage";
+import KomunitasPage from "./pages/KomunitasPage";
 
-function App() {
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/individu" element={<IndividuPage />} />
+      <Route path="/komunitas" element={<KomunitasPage />} />
+    </Routes>
+  </Router>
+);
 
-	return (
-		<>
-			<h1 className='text-black'>Kocak</h1>
-		</>
-	)
-}
-
-export default App
+export default App;
