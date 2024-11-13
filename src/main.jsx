73c3ed76e1a566937ae9508af68465a6fcd19c9f
adhimrahman <adhimrahaman@gmail.com>
2024-11-13 23:56:27 +0800

    fix :

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import ProfileDashboard from './pages/profileDashboard';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ProfileDashboard />
-  </StrictMode>
-);
-=======
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/index.css'
@@ -20,4 +8,3 @@ createRoot(document.getElementById('root')).render(
 		<App />
 	</React.StrictMode>,
 )
->>>>>>> 7912c054e68aa14a7b507bf7ef76d4960c1ad307
--- src/pages/profileDashboard.jsx
+++ src/pages/profileDashboard.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 
-<<<<<<< HEAD
 const ProfileDashboard = () => {
   return (
     <div className="flex h-screen font-sans text-gray-800">
@@ -23,17 +21,6 @@ const ProfileDashboard = () => {
           </nav>
           <div className="text-2xl">👤</div>
         </header>
-=======
-function ProfileDashboard() {
-    console.log("Signup loaded");
-
-	return(
-		<>
-			<h1 className="text-black">ProfileDashboard</h1>
-		</>
-	)
-}
->>>>>>> 7912c054e68aa14a7b507bf7ef76d4960c1ad307
 
         {/* Profile Section */}
         <div className="profile-section bg-gray-50 p-6 rounded-lg shadow-md mt-[80px]">
